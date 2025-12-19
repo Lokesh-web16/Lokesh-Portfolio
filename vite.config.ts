1ae95b2a34658182bd5b9c45c8dcffe0006bb835
Lokesh T <lokeshnaikar7@gmail.com>
2025-12-19 10:04:29 +0530

    Update vite config with correct repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Base path must match your repository name: Lokesh-Portfolio-Web
-  base: "/Lokesh-Portfolio-Web/",
+  base: "/Lokesh-Portfolio/",
   plugins: [react()],
 });
